8209d940d61337f38907f562ed62d241b53daeeb
MSDN-SPECIAL\Administrator <sting852@naver.com>
2017-04-16 13:11:46 +0900

    두번째 카피본 상단메뉴 만든것!과 0416에 한것

--- Project01_copy/js/script.js
+++ Project01_copy/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     
-    $('.box').on('click',function(){
-        alert('박스');
+    $('.slider').slick({
+        
     });
+    
+    $('.boygirl li').on('mouseover',function(){
+        $(this).css('box-shadow','1px 1px 2px #e33333');
+    });
+    
 });
