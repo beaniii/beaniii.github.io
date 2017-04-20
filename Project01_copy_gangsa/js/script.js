a9ee8645901be2cb4722497f5ffd98ada0f1e9b1
MSDN-SPECIAL\Administrator <sting852@naver.com>
2017-04-20 14:49:10 +0900

    0420슬릭추가

--- Project01_copy_gangsa/js/script.js
+++ Project01_copy_gangsa/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+    $('.center .center_top .slider').slick({
+         
+    });
+    
     $('.nav .nav_right li').on('mouseover',function(){
         
     });
-    
-    
 });
