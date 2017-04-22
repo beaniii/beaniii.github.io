105be9f36fcd2890bfe2a1a3e1143f1c2eacb004
MSDN-SPECIAL\Administrator <sting852@naver.com>
2017-04-22 13:18:50 +0900

    0422학원자료

--- Project01_copy_gangsa/js/script.js
+++ Project01_copy_gangsa/js/script.js
@@ -3,7 +3,21 @@ $(document).ready(function(){
          
     });
     
-    $('.nav .nav_right li').on('mouseover',function(){
+    $('.nav .nav_right #home').hover(function(){
+        $('.center .center_top #home').addClass('appear');
+    });
+    $('.nav .nav_right #home').on('mouseleave',function(){
+        $('.center .center_top #home').removeClass('appear');
+    });
+    $('.nav .nav_right #home').siblings().hover(function(){
         
     });
+    
+    
+    
+    
+//    $('').on('click',function(){
+//        alert('3') ;
+//    });
+    
 });
