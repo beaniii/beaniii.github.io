0713d8ffddd07468110d18058494c45c605bac10
MSDN-SPECIAL\Administrator <sting852@naver.com>
2017-04-25 15:10:24 +0900

    0425

--- Project01_copy_gangsa - 복사본 - 0424/js/script.js
+++ Project01_copy_gangsa - 복사본 - 0424/js/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
     
     
+    $('.slider').slick({
+        
+    });
+    
+     
+   $(window).on('scroll',function(){
+		
+		var nowScroll = $(this).scrollTop(); // 스크롤 현재 위치
+		
+        var menuPos = nowScroll;
+       
+       $('.menubar').css('top',menuPos+'px');
+       
+       
+	});
+    
     
     
     
